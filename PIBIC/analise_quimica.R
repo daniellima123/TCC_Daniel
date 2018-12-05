fb0a86ed572f2f4dadd161996943933c0188f6ef
daniel <daniel.limaviegas454@gmail.com>
2018-12-05 15:13:37 -0200

    imagens para a apresentação

--- PIBIC/analise_quimica.R
+++ PIBIC/analise_quimica.R
@@ -20,7 +20,7 @@ teste_hipo(opt)
 
 
 
-betas <- opt$par[-1]
+betas <- opt_qu$par[-1]
 model_vars <- df[, c('x0', 'Sexo', 'Turno', 'Ingresso', 'Idade')]
 model_vars <- d[, c('x0', 'idade', 'tratamento')]
 x_b <- as.matrix(model_vars)%*%betas
@@ -30,6 +30,22 @@ e <- H_t(tempo = d[['tempo']], beta = opt$par[1], mu = mu)
 
 s <- Surv(e, d[['censura']])
 km <- survfit(s ~ 1, d)
+a <- ggsurvplot(km,conf.int = F, col = 'red', legend='none')
+a$plot + 
+  geom_line(aes(x = a$plot$data$time,
+                exp(-a$plot$data$time)))+xlab('Resíduo')+ylab('S(t)')+
+  theme_bw()+
+  annotate("rect", xmin = 1.2, xmax = 1.6, ymin = 0.75, ymax = 0.95, fill = 'white')+
+  annotate('segment', x = 1.2, xend = 1.28, y = 0.93, yend = 0.93, color = 'red', size = 1)+
+  annotate('text', x = 1.45,  y = 0.93, label = 'Kaplan-Meier', size=3.3)+
+  annotate('text', x = 1.485, y = 0.88, label = 'Função Estimada', size=3.3)+
+  annotate('segment', x = 1.2, xend = 1.28, y = 0.88, yend = 0.88, color = 'black', size = 1)
+
+mu <- exp(x_b)
+e <- H_t(tempo = df[['Tempo']], beta = opt$par[1], mu = mu)
+
+s <- Surv(e, df[['Status']])
+km <- survfit(s ~ 1, df)
 a <- ggsurvplot(km,conf.int = F, col = 'red', legend='none')
 a$plot + 
   geom_line(aes(x = a$plot$data$time,
--- programacao/analise_vitamina_tcc.R
+++ programacao/analise_vitamina_tcc.R
@@ -54,8 +54,8 @@ l_group <- function(par, df, interval, s, censura, tempo){
 }
 
 
-l_reg <- function(par, df, interval, s, censura, tempo){
-  alpha <- exp(as.matrix(df[,c('x0', 'idade', 'tratamento')])%*%par[-1])
+l_reg <- function(par, df, interval, s, censura, tempo, expl){
+  alpha <- exp(as.matrix(df[,expl])%*%par[-1])
   gam <- par[1]
   val <- c()
   for(i in seq_len(nrow(interval))){
@@ -85,7 +85,7 @@ l_reg <- function(par, df, interval, s, censura, tempo){
 l_reg1 <- function(par, df, interval, s, censura, tempo, expl){
   alpha <- exp(as.matrix(df[,expl])%*%par[-1])
   gam <- par[1]
-  data_mod <- data.frame(tempo = df[[tempo]], cens = d[[censura]], alpha = alpha)
+  data_mod <- data.frame(tempo = df[[tempo]], cens = df[[censura]], alpha = alpha)
   va <- sapply(1:nrow(int_df), function(x){
     v <- ifelse(data_mod$tempo < int_df$inf[x], 0,
                 ifelse(data_mod$tempo < int_df$sup[x] & data_mod$cens == 1,
@@ -105,14 +105,21 @@ l_reg1 <- function(par, df, interval, s, censura, tempo, expl){
   
 }
 
+opt_qu <- optim(par = c(1, 2, 1, -1, -1, 0), df=df, interval=int_df, s = sobrev, tempo = 'Tempo',
+                censura='Status', expl = c('x0', 'Sexo', 'Turno', 'Ingresso', 'Idade'), fn = l_reg1)
+
+opt_quim_vazio <- optim(par = c(1, 2), l_group, df = df, interval = int_df, s = sobrev, tempo = 'Tempo',
+                        censura = 'Status', hessian = T)
+
+
 opt_vazio <- optim(par = c(1, 2), l_group, df = d, interval = int_df, s = sobrev, tempo = 'tempo',
                    censura = 'censura', hessian = T)
 
 opt_1 <- optim(par = c(1.65, 2.08, 0.27, -0.84), l_reg, df = d, interval = int_df, s = sobrev,
              tempo = 'tempo', censura = 'censura', hessian = T)
 Sys.time()
-opt <- optim(par = c(1.65, 2.08, 0.27, -0.84, 2), l_reg1, df = d, interval = int_df, s = sobrev,
-             tempo = 'tempo', censura = 'censura', hessian = T, expl = c('x0', 'idade', 'tratamento', 'sexo'))
+opt <- optim(par = c(1.65, 2.08, 0.27, -0.84), l_reg1, df = d, interval = int_df, s = sobrev,
+             tempo = 'tempo', censura = 'censura', hessian = T, expl = c('x0', 'idade', 'tratamento'))
 Sys.time()
 mu_semestre <- opt$par[1]
 beta_semestre <- opt$par[2]
@@ -122,11 +129,18 @@ beta_ano <- opt$par[2]
 alpha <- opt_vazio$par[1]
 gamma <- opt_vazio$par[2]
 km <- surv_fit(Surv(d$tempo, d$censura) ~ 1, data = d)
-a <- ggsurvplot(km, conf.int = F, color = 'red', legend = 'none',
-                ggtheme = theme_bw())
+a <- ggsurvplot(km, conf.int = F, color = 'red', legend = c(0.85, 0.85),
+                ggtheme = theme_bw(), legend.labs = c('Kaplan-Meier'))
+ggsurvplot(km, data = d, conf.int = F, palette = 'red', legend = c(0.85, 0.85),
+           ggtheme = theme_bw(), legend.labs = c('Kaplan-Meier'), legend.title = 'Curvas')
 a$plot+
   geom_line(aes(x = a$plot$data$time, sobrev(a$plot$data$time, alpha, gamma)))+
-  xlab('tempo')+ylab('S(t)')
+  xlab('Tempo')+ylab('S(t)')+
+  annotate("rect", xmin = 150, xmax = 190, ymin = 0.75, ymax = 0.95, fill = 'white')+
+  annotate('segment', x = 150, xend = 158, y = 0.93, yend = 0.93, color = 'red', size = 1)+
+  annotate('text', x = 179, y = 0.93, label = 'Kaplan-Meier', size=3.3)+
+  annotate('text', x = 184, y = 0.88, label = 'Função Estimada', size=3.3)+
+  annotate('segment', x = 150, xend = 158, y = 0.88, yend = 0.88, color = 'black', size = 1)
 
 qplot(d$tempo, geom='histogram', binwidth=1)+theme_bw()+xlab('Tempo')+ylab('Frequência')
 ggsurv
